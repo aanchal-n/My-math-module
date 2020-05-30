162b773374f6449345adcc37464d7a275ba2c958
aanchal-n <aanchalnarendran@gmail.com>
2020-05-30 13:08:19 +0530

    Made code more readable and added triangle functionality

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # My-math-module
 It is a math module that calculates various geometrical values such as area, perimeter, volume etc based on the users input
-It is currently designed to compute circumference/perimeter and area for a circle,square and rectangle. 
-It can also compute volume and surface area for a sphere, cube, cuboid, cylinde, cone, pyramid and prism. 
+It is currently designed to compute circumference/perimeter and area for a circle,square,rectangle and square. 
+It can also compute volume and surface area for a sphere, cube, cuboid, cylinde, cone and pyramid. 
 For now, it's a terminal run program. 
 I will be adding gui to it if I decide to work on it further. 
 This was developed as a part of my project for a university course. 
--- mymath.py
+++ mymath.py
@@ -6,199 +6,228 @@ Created on Tue Nov 19 11:21:36 2019
 @author: aanchalnarendran
 """
 
-def circlecirc(r):
-    circum= 2*3.14*r
-    return circum
-def circlearea(r):
-    a=3.14*(r**2)
-    return a
-def squarearea(a):
-    area=a*a
+def circle_circumference_calc(radius):
+    circumference = 2*3.14*r
+    return circumference
+    
+def circle_area_calc(radius):
+    area = 3.14*(radius**2)
     return area
-def squareper(a):
-    per=4*a
-    return per
-def rectarea(l,b):
-    area=l*b
+    
+def square_area_calc(side):
+    area = side*side
     return area
-def rectperm(l,b):
-    per=2*(l+b)
+    
+def square_perimeter_calc(side):
+    perimeter = 4*side
     return per
-def spherev(r):
-    v=(4*3.14*(r**3))/4
-    return v
-def spheresa(r):
-    sa=4*3.14*(r**2)
-    return sa
-def cubevol(a):
-    v=a**3
-    return v
-def cubesa(a):
-    v=6*(a**2)
-    return v
-def cuboidsa(a,b,c):
-    sa=2*(a*b + b*c + a*c)
-    return sa
-def cuboidvol(a,b):
-    v=a*a*b
-    return v
-def cylinderv(r,h):
-    v=3.14*(r**2)*h
-    return v
-def cylindersa(r,h):
-    sa=3.14*(r**2)+ 3.14*r*((r**2 + h**2)**0.5)
-    return sa
-def conesa(r,s):
-    sa=3.14*(r**2)+3.14*r*s
-    return sa
-def conevol(r,s):
-    v= (3.14*(r**2)*s)/3
-    return v
-def pyramidarea(b,h,s):
-    sa=2*b*s + b**2
-    return sa
-def pyramidvolume(b,h):
-    v=((b**2)*h)/3
-    return v
-def prismsa(b,h,l,s):
-    sa= b*h + 2*l*s + l*b
-    return sa
-def prismvol(b,h,l):
-    vol=b*h*l
-    return vol
+    
+def rectangle_area_calc(length,breadth):
+    area = length*breadth
+    return area
+    
+def rectangle_perimeter(length,breadth):
+    perimeter = 2*(length+breadth)
+    return perimeter
+    
+def sphere_volume(radius):
+    volume = (4*3.14*(radius**3))/4
+    return volume
+    
+def sphere_surface_area(radius):
+    surface_area = 4*3.14*(radius**2)
+    return surface_area
+    
+def cube_volume(side):
+    volume = side**3
+    return volume
+    
+def cube_surface_area(side):
+    surface_area = 6*(side**2)
+    return surface_area
+    
+def cuboid_surface_area(length,breadth,height):
+    surafce_area = 2*(length*breadth + breadth*height + length*height)
+    return surface_area
+    
+def cuboid_volume(length,breadth,height):
+    volume = length*breadth*height
+    return volume
+    
+def cylinder_volume(radius,height):
+    volume = 3.14*(radius**2)*height
+    return volume
+    
+def cylinder_surface_area(radius,height):
+    surface_area = 2*3.14*(radius**2)+ 3.14*radius*((radius**2 + height**2)**0.5)
+    return surface_area
+    
+def cone_surface_area(radius,slant_height):
+    surface_area = 3.14*(radius**2)+3.14*radius*((radius**2 + height**2)**0.5)
+    return surface_area
+    
+def cone_volume(radius,height):
+    volume = (3.14*(radius**2)*height)/3
+    return volume
+    
+def pyramid_area(length,width,height):
+    surface_area = lenght*width + length*(((width/2)**2 + height**2)**0.5) + width*(((length/2)**2 + height**2)**0.5)
+    return surface_area
+    
+def pyramid_volume(length,width,height):
+    volume = length*width*height/3
+    return volume
+    
+def triangle_perimeter(side1,side2,side3):
+    perimeter = side1 + side2 + side3
+    return perimeter
+    
+def triangle_area(side1, side2, side3):
+    perimeter_half = (triangle_perimeter(side1,side2,side3))/2
+    area = (p*(p-side1)*(p-side2)*(p-side3))**0.5
+    return area
+    
 def main():
     print("welcome to my math module")
-    print("For circle related computation,enter1")
-    print("For square related computation,enter2")
-    print("For rectangle related computation,enter3")
-    print("For sphere related computation,enter4")
-    print("For cube related computation,enter5")
-    print("For cuboid related computation,enter6")
-    print("For cylinder related computation,enter7")
-    print("For cone related computation,enter8")
-    print("for pyramid related computation,enter9")
-    print("for prism related computation,enter10")
-    n=int(input("enter your choice"))
-    if n==1:
-        r=int(input("enter the radius"))
-        print("to compute cicrcumference,enter1")
-        print("to compute area, print2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("circumference is",circlecirc(r))
-        elif e==2:
-            print("area is",circlearea(r))
+    print("For circle related computation,enter 1")
+    print("For square related computation,enter 2")
+    print("For rectangle related computation,enter 3")
+    print("for triangle related computation,enter 4")
+    print("For sphere related computation,enter 5")
+    print("For cube related computation,enter 6")
+    print("For cuboid related computation,enter 7")
+    print("For cylinder related computation,enter 8")
+    print("For cone related computation,enter 9")
+    print("for pyramid related computation,enter 10")
+    choice = int(input("enter your choice"))
+    
+    if choice == 1:
+        radius_val = int(input("enter the radius"))
+        print("to compute cicrcumference,enter 1")
+        print("to compute area, print 2")
+        internal_choice = int(input("enter your choice"))
+        if internal_choice == 1:
+            print("circumference is",circle_circumference_calc(radius_val))
+        elif internal_choice == 2:
+            print("area is",circle_area_calc(radius_val))
         else:
             print("wrong option")
-    elif n==2:
-        r=int(input("enter the side"))
+            
+    elif choice == 2:
+        side = int(input("enter the side"))
         print("to compute perimeter,enter 1" )
         print("to compute area,enter 2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("perimeter is",squareper(r))
-        elif e==2:
-            print("area is",squarearea(r))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("perimeter is",square_perimeter_calc(side))
+        elif internal_choice==2:
+            print("area is",square_area_calc(side))
         else:
             print("wrong option")
-    elif n==3:
-        l=int(input("enter the length"))
-        b=int(input("enter the breadth"))
-        print("to compute perimeter, enter1")
+            
+    elif choice==3:
+        length = int(input("enter the length"))
+        breadth = int(input("enter the breadth"))
+        print("to compute perimeter, enter 1")
         print("to compute area, enter 2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("perimeter is",rectperm(l,b))
-        elif e==2:
-            print("area is", rectarea(l,b))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("perimeter is",rectangle_perimeter(length,breadth))
+        elif internal_choice==2:
+            print("area is", rectangle_area_calc(length,breadth))
         else:
             print("wrong option")
-    elif n==4:
-        r=int(input("enter radius"))
+            
+    elif choice==4:
+        side1 = int(input("enter side 1"))
+        side2 = int(input("enter side 2"))
+        side3 = int(input("enter side 3"))
+        print("for computation of perimeter, enter 1")
+        print("for computation of area,enter 2")
+        internal_choice = int(input("Enter your choice"))
+        if internal_choice == 1:
+            print("the perimeter is",triangle_perimeter(side1,side2,side3))
+        elif internal_choice == 2:
+            print("the area is",triangle_area(side1,side2,side3))
+        else:
+            print("wrong option")
+            
+    elif choice==5:
+        radius = int(input("enter radius"))
         print("for computation of area, enter1")
         print("for computation of area, enter 2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("the area is",spheresa(r))
-        elif e==2:
-            print("the volume is",spherev(r))
+        internal_choice=int(input("enter your choice"))
+        if internal_choice==1:
+            print("the area is",sphere_surface_area(r))
+        elif internal_choice==2:
+            print("the volume is",sphere_volume(r))
         else:
             print("wrong choice")
-    elif n==5:
-        a=int(input("enter the side"))
+            
+    elif choice==6:
+        side = int(input("enter the side"))
         print("to compute surface area, enter 1")
         print("to compute volume,enter 2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("the surface area of cube",cubesa(a))
-        elif e==2:
-            print("the volume is",cubevol(a))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("the surface area of cube",cube_surface_area(a))
+        elif internal_choice==2:
+            print("the volume is",cube_volume(a))
         else:
             print("wrong choice")
-    elif n==6:
-        l=int(input("enter the length"))
-        b=int(input("enter the breadth"))
-        h=int(input("enter the height"))
+            
+    elif choice==7:
+        len = int(input("enter the length"))
+        bre = int(input("enter the breadth"))
+        hei = int(input("enter the height"))
         print("to compute surface area,enter 1")
         print("to compute volume, enter 2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("the surface area is", cuboidsa(l,b,h))
-        elif e==2:
-            print("the volume is",cuboidvol(l,b,h))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("the surface area is", cuboid_surface_area(len,bre,hei))
+        elif internal_choice==2:
+            print("the volume is",cuboid_volume(len,bre,hei))
         else:
             print("wrong choice")
-    elif n==7:
-        r=int(input("enter radius"))
-        h=int(input("enter height"))
+            
+    elif choice==8:
+        radius = int(input("enter radius"))
+        height = int(input("enter height"))
         print("to compute surface area,enter 1")
         print("to compute volume, enter2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("surface area is",cylindersa(r,h))
-        elif e==2:
-            print("volume is",cylinderv(r,h))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("surface area is",cylinder_surface_area(radius,height))
+        elif internal_choice==2:
+            print("volume is",cylinder_volume(radius,height))
         else:
             print("wrong choice")
-    elif n==8:
-        r=int(input("enter the radius"))
-        s=int(input("enter the side"))
+            
+    elif choice==9:
+        radius = int(input("enter the radius"))
+        height = int(input("enter the height"))
         print("to compute surface area,enter1")
         print("to compute volume,enter2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("the surface area is",conevol(r,s))
-        elif e==2:
-            print("the volume is",conevol(r,s))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("the surface area is",cone_surface_area(radius,height))
+        elif internal_choice==2:
+            print("the volume is",cone_volume(radius,height))
         else:
             print("wrong choice")
-    elif n==9:
-        b=int(input("enter base"))
-        h=int(input("enter height"))
-        s=int(input("enter side"))
+    elif choice==10:
+        b_len = int(input("enter base length"))
+        b_wid = int(input("enter base width"))
+        height = int(input("enter height"))
         print("top compute surface area,enter 1")
         print("to compute volume,enter2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("surface area is",pyramidarea(b,h,s))
-        elif e==2:
-            print("volume is",pyramidvolume(b,h))
+        internal_choice = int(input("enter your choice"))
+        if internal_choice==1:
+            print("surface area is",pyramid_area(b_len,b_wid,height))
+        elif internal_choice==2:
+            print("volume is",pyramid_volume(b_len,b_wid,height))
         else:
             print("wrong choice")
-    elif n==10:
-        b=int(input("enter the base"))
-        h=int(input("enter the height"))
-        l=int(input("enter the length"))
-        s=int(input("enter the side"))
-        print("to compute surface area,enter1")
-        print("to compute volume,enter 2")
-        e=int(input("enter your choice"))
-        if e==1:
-            print("the surface area is", prismsa(b,h,l,s))
-        elif e==2:
-            print("the volume is",prismvol(b,h,l))
-        else:
-            print("wrong option")
     else:
         print("wrong option")
 main()
